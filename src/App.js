652d72076d85b22303040336885706e137d40e29
teoharianto <teofilushendry5@gmail.com>
2020-10-31 08:12:56 +0700

    v2.1

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = (props) => {
                   className={classnames({ active: activeTab === '1' })}
                   onClick={() => { toggle('1'); }}
                 >
-                  Rukun Raharja
+                  Triguna
           </NavLink>
               </NavItem>
               <NavItem>
@@ -68,7 +68,7 @@ const App = (props) => {
                 >
                   TMSI
           </NavLink>
-          
+
               </NavItem>
               <NavItem>
                 <NavLink
@@ -77,7 +77,7 @@ const App = (props) => {
                 >
                   OJK
           </NavLink>
-          
+
               </NavItem>
             </Nav>
             <TabContent activeTab={activeTab}>
@@ -85,40 +85,72 @@ const App = (props) => {
                 <Row>
                   <Col sm="12">
                     <div className="tabsLayout">
-                    <h2>Rukun Raharja</h2>
-                    <p style={{marginTop: "0rem", fontSize: "14px"}}>January 3, 2014 - Oktober 20, 2017</p>
-                    <p>Di perusahaan ini saya bertugas sebagai document control yang terlibat dalam beberapa proyek.</p>
-                    <hr />
-                    <p><strong>Pembangunan pipa 20" Petronas Carigali - PJB UP Gresik</strong></p>
-                    <p>Peran saya adalah memastikan dokumen yang digunakan update dan membuat MDR</p>
+                      <h2>Triguna Internusa Pratama</h2>
+                      <p style={{ marginTop: "0rem", fontSize: "14px" }}>January 3, 2014 - Oktober 20, 2017</p>
+                      <h5>Project Document Controller</h5>
+                      <p>Responsible for carrying out the following functions:</p>
+                      <ul>
+                        <li>Input and update status document (correspondence, engineering document, QC
+document and drawing)</li>
+                        <li>Circulate document for approval from particular engineer and Project Manager.</li>
+                        <li>Prepare and track document transmittals.</li>
+                        <li>Ensure that the most current revision of controlled documents is available for use by
+Construction.</li>
+                        <li>Establish, and maintain the status register and controlled document distribution list.</li>
+                        <li>Administer the control of documentation for Construction. This includes receipt, logging,
+                        reproduction, distribution, filing, and auditing of the documents and interim change
+documents.</li>
+                      </ul>
                     </div>
                   </Col>
                 </Row>
               </TabPane>
               <TabPane tabId="2">
                 <Row>
-                <Col sm="12">
+                  <Col sm="12">
                     <div className="tabsLayout">
-                    <h2>Teknoglobal Multi Sistem Integrasi</h2>
-                    <p style={{marginTop: "0rem", fontSize: "14px"}}>January 3, 2018 - December 14, 2019</p>
-                    <p>Di Teknoglobal saya melakukan sesuatu yang berkaitan dengan ETL dan saya juga terlibat dalam beberapa proyek.</p>
-                    <hr />
-                    <p><strong>Danamon FIRMS Project</strong></p>
-                    <p>Peran saya adalah mengembangkan ETL bersama tim.</p>
+                      <h2>Teknoglobal Multi Sistem Integrasi</h2>
+                      <p style={{ marginTop: "0rem", fontSize: "14px" }}>January 3, 2018 - December 14, 2019</p>
+                      <h5>Software Quality Assurance <small style={{ fontSize: "12px" }}>Mar 2019 – Oct 2019</small></h5>
+                      <ul>
+                        <li>Monitors every phase of the development process to ensure that the design and
+software adhere to company standards.</li>
+                        <li>Identify bugs and potential problems</li>
+                        <li>Plan and perform different stages of testing</li>
+                        <li>Develop new tools technology and testing processes</li>
+                        <li>Make document test progress and result</li>
+                      </ul>
+                      <h5>Junior ETL/SQL Developer <small style={{ fontSize: "12px" }}>Jan 2018-Apr 2019</small></h5>
+                      <ul>
+                        <li>Developed and tested extraction, transformation, and load (ETL) processes.</li>
+                        <li>Designed and developed SSIS and Sessions based on business user requirements and
+business rules to load data from source flat files and SQL tables to target tables.</li>
+                        <li>Designed and implemented stored procedures, views and other application database
+code objects to aid mappings.</li>
+                        <li>Maintained SQL scripts and complex queries for analysis and extraction.</li>
+                        <li>Provided guidance to campaign analyst on complex technical projects that required
+SQL coding</li>
+                      </ul>
                     </div>
                   </Col>
                 </Row>
               </TabPane>
               <TabPane tabId="3">
                 <Row>
-                <Col sm="12">
+                  <Col sm="12">
                     <div className="tabsLayout">
-                    <h2>Indonesian Financial Services Authority (Otoritas Jasa Keuangan)</h2>
-                    <p style={{marginTop: "0rem", fontSize: "14px"}}>December 15, 2019 - Now</p>
-                    <p>Di OJK saya melakukan sesuatu yang berkaitan dengan data dan saya juga terlibat dalam beberapa proyek.</p>
-                    <hr />
-                    <p><strong>Metadata Management Project</strong></p>
-                    <p>Peran saya adalah membuat struktur metadata dan skenarionya.</p>
+                      <h2>Indonesian Financial Services Authority (Otoritas Jasa Keuangan)</h2>
+                      <p style={{ marginTop: "0rem", fontSize: "14px" }}>December 15, 2019 - Now</p>
+                      <h5>Research Fellow Business Analyst (Contract)</h5>
+                      <ul>
+                        <li>Assist Analyst to inform Financial Sector Data for Member of the Board of
+Commissioners Otoritas Jasa Keuangan.</li>
+                        <li>Providing assistance to analysts in developing an application such as creating
+                        user requirements and attachments, IS / Application modules, studies, and mapping
+of an application coordination between users and IT.</li>
+                        <li>Doing User Acceptance Testing as identify bugs and potential problems, plan and
+perform different stages of testing, make document test progress and result.</li>
+                      </ul>
                     </div>
                   </Col>
                 </Row>
